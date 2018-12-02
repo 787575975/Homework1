f2d687432faef52a8984ed963c41b2c4f1673f04
DOLADAOUD <doladaoud710@gmail.comIbrahim-99git config  --global  user.email doladaoud710@gmail.com>
2018-12-02 08:55:54 -0500

    about Project-4

---
+++ Hw 1 Name & Address/src/hw1/Homework.java
@@ -0,0 +1,27 @@
+package hw1;
+
+public class Homework {
+
+public static void main(String[] args){
+			
+			String first_name= "Dola";
+			String last_name= "Daoud";
+			String student_id= "3778ny";
+			String address= "474 48th ave Lic";
+			System.out.print("My name is" +first_name +last_name);
+			System.out.print("My student id is "+ student_id);
+			System.out.print("I live in" +address);
+				
+			
+}
+			
+			                                                                       
+			                                   
+			                                    
+			
+			
+	
+	
+	
+	
+}
---
+++ jiojojko/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dola
+ *
+ */
+module jiojojko {
+}
